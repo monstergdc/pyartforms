0de8c0d3fac775ecc5803732a68a7e8684b20ab8
Jakub Noniewicz, MoNsTeR of GDC / Noniewicz.com <32686195+monstergdc@users.noreply.github.com>
2018-05-05 20:01:05 +0200

    waves update

--- playgroud/waves.py
+++ playgroud/waves.py
@@ -1,23 +1,22 @@
 
 # paint algorithms (artificial artist), v1.0, Python version
 # #1 waves
-# (c)2017-2018 MoNsTeR/GDC, Noniewicz.com, Jakub Noniewicz
+# (c)2018 MoNsTeR/GDC, Noniewicz.com, Jakub Noniewicz
 # cre: 20180505
 # upd: 201805??
 
 # see:
 # https://pillow.readthedocs.io/en/3.1.x/reference/ImageDraw.html
-# http://pillow.readthedocs.io/en/5.1.x/reference/ImageFilter.html
 
 # TODO:
 # - ?
 
 
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw
 import random, math, string, os, sys
 from bezier import make_bezier
 from datetime import datetime as dt
-from drawtools import get_canvas, circle, box, triangle, gradient, gradient2
+from drawtools import *
 
 
 def waves1(params, fn):
@@ -46,7 +45,7 @@ def waves1(params, fn):
         aofs3 = random.randint(0, 360)
         aofs4 = random.randint(0, 360)
         fx = 100
-        fd = 100
+        fd = 100*params['f0']
         fofs1 = random.randint(0, fx)/fd
         fofs2 = random.randint(0, fx)/fd
         fofs3 = random.randint(0, fx)/fd*2
@@ -71,10 +70,10 @@ def waves1(params, fn):
 # ---
 
 def do_waves1(cnt, w, h, odir):
-
     params1 = {
         'w': w, 'h': h, 'bg': (224, 244, 0),
         'z': 14,
+        'f0': 1,
         'horizontal': False,
         'gradient': 256,
         'c1': (0,0,255),
@@ -84,6 +83,7 @@ def do_waves1(cnt, w, h, odir):
     params2 = {
         'w': w, 'h': h, 'bg': (224, 244, 0),
         'z': 12,
+        'f0': 1,
         'horizontal': True,
         'gradient': 256,
         'c1': (0,0,0),
@@ -93,17 +93,39 @@ def do_waves1(cnt, w, h, odir):
     params3 = {
         'w': w, 'h': h, 'bg': (0, 0, 0),
         'z': 9,
+        'f0': 1,
         'horizontal': False,
         'gradient': 24,
         'c1': (0,0,0),
         'c2': (255,0,0),
         'c3': (255,255,0),
     }
-
+    params4 = {
+        'w': w, 'h': h, 'bg': (0, 0, 0),
+        'z': 4,
+        'f0': 0.33,
+        'horizontal': True,
+        'gradient': 256,
+        'c1': (255,0,0),
+        'c2': (0,255,0),
+        'c3': (0,0,255),
+    }
+    params5 = {
+        'w': w, 'h': h, 'bg': (0, 0, 0),
+        'z': 4,
+        'f0': 3.0,
+        'horizontal': True,
+        'gradient': 256,
+        'c1': (255,0,0),
+        'c2': (0,255,0),
+        'c3': (0,0,255),
+    }
     for n in range(cnt):
         waves1(params1, odir+'waves1-%dx%d-01-%03d.png' % (w, h, n+1))
         waves1(params2, odir+'waves1-%dx%d-02-%03d.png' % (w, h, n+1))
         waves1(params3, odir+'waves1-%dx%d-03-%03d.png' % (w, h, n+1))
+        waves1(params4, odir+'waves1-%dx%d-04-%03d.png' % (w, h, n+1))
+        waves1(params5, odir+'waves1-%dx%d-05-%03d.png' % (w, h, n+1))
 
 # ---
 
@@ -111,10 +133,7 @@ def main():
     start_time = dt.now()
     cnt = 5
     ca = get_canvas('A3')
-    w = ca[0]
-    h = ca[1]
-    odir = ''
-    do_waves1(cnt, w, h, odir)
+    do_waves1(cnt, ca[0], ca[1], '')
     time_elapsed = dt.now() - start_time
     print('ALL done. elapsed time: {}'.format(time_elapsed))
 
