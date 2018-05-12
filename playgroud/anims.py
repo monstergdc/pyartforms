2b7b56c444b037abd2ae0de47c5be13f431ad9bd
Jakub Noniewicz, MoNsTeR of GDC / Noniewicz.com <32686195+monstergdc@users.noreply.github.com>
2018-05-12 15:24:41 +0200

    added anim#4

--- playgroud/anims.py
+++ playgroud/anims.py
@@ -3,16 +3,19 @@
 # (c)2018 MoNsTeR/GDC, Noniewicz.com, Jakub Noniewicz
 # anim#1/anim#2 - recreated from seeing as in some GIFs I've once seen (so concept isn't mine)
 # cre: 20180505
-# upd: 20180506
+# upd: 20180506, 12
 
 # orig GIFs md5:
 # #1: ad2bde22541ac1b05b2c08fd805ebafe *001.gif
 # #2: 927339ac93af260455892d15e0f1f5c3 *CM128.gif
+# #3: ?
+# #4: 7dafd915911faf3e14678fefe163d7f1 *DSC58.gif
 
 # see: https://pillow.readthedocs.io/en/3.1.x/reference/ImageDraw.html
 
 # TODO:
-# - ?
+# - fin anim #3 proper
+# - fin anim #4
 
 
 from PIL import Image, ImageDraw, ImageFilter
@@ -20,6 +23,7 @@ import random, math, os, sys
 import cv2
 from drawtools import *
 
+image = 'tmp.png'   #tmp img file
 c = math.pi/180
 s2 = math.sqrt(2)/2
 
@@ -140,14 +144,148 @@ class Anim2():
         im.save(image)
         self.dt += 1
 
+# http://aleph.se/andart2/math/fractals-and-steiner-chains/
+# https://en.wikipedia.org/wiki/Steiner_chain
+
+class Anim3():
+    # 400: 240/160 == 3/5 do 2/5 z 5 -> 160: 100/60
+    #z = 1/5
+
+    def _init__(self):
+        self.init((0, 0), 0, 0)
+
+    def init(self, canvas, level, width):
+        self.dt = 0
+        self.canvas = canvas
+        self.level = level
+        self.width = width
+        self.x0 = self.canvas[0]/2
+        self.y0 = self.canvas[1]/2
+        self.r0 = (self.canvas[0]-16)/2
+
+    def mycir000(self, draw, x, y, r, w):
+        pts = []
+        lim = 360*4
+        for a in range(lim):
+            xc = x + r * math.cos(c*a/lim*360)
+            yc = y + r * math.sin(c*a/lim*360)
+            pts.extend((xc, yc))
+        draw.line(pts, fill=(0, 0, 0), width=w)
+
+    def mycir(self, draw, x, y, r, w):
+        color = (0, 0, 0)
+        circle(draw, x, y, r, None, color)
+        for rx in range(int(w/2)):
+            circle(draw, x, y, r+rx*0.5, None, color)
+            circle(draw, x, y, r-rx*0.5, None, color)
+
+    def drawlevel(self, draw, level, x1, y1, r1):
+        if level == 0:
+            return
+        self.mycir(draw, x1, y1, r1, self.width)
+
+        x = x1
+        y = y1
+        r = r1
+        self.drawlevel(draw, level-1, x, y, r)
+        x = x1 - r1 * 2/5
+        y = y1
+        r = r1 * 3/5
+        self.drawlevel(draw, level-1, x, y, r)
+        x = x1 + r1 - r1 * 2/5 * 3/5 - r1 * 2/5 * 2/5 * 2
+        y = y1
+        r = r1 * 2/5 * 3/5
+        self.drawlevel(draw, level-1, x, y, r)
+        x = x1 + r1 - r1 * 2/5 * 2/5
+        y = y1
+        r = r1 * 2/5 * 2/5
+        self.drawlevel(draw, level-1, x, y, r)
+
+        x = x1 + r1 * 2/5 * 4/5 + r1 * 1/5 * 1/5 * 0.6 # approx!
+        y = y1 - r1 * 3/5 + r1 * 1/5 * 2/5 - r1 * 1/5 * 1/5 * 1.18 # approx!
+        r = r1 * 2/5 * 4/5 + r1 * 1/5 * 1/5 * 0.4 # approx!
+        self.drawlevel(draw, level-1, x, y, r)
+
+        x = x1 + r1 * 2/5 * 4/5 + r1 * 1/5 * 1/5 * 0.6 # approx!
+        y = y1 + r1 * 3/5 - r1 * 1/5 * 2/5 + r1 * 1/5 * 1/5 * 1.18 # approx!
+        r = r1 * 2/5 * 4/5 + r1 * 1/5 * 1/5 * 0.4 # approx!
+        self.drawlevel(draw, level-1, x, y, r)
+
+
+    def drawframe(self, image):
+        im = Image.new('RGB', self.canvas, (255, 255, 255))
+        draw = ImageDraw.Draw(im)
+
+        self.drawlevel(draw, self.level, self.x0, self.y0, self.r0)
+
+##        x = self.x0
+##        y = self.y0
+##        r = self.r0
+##        self.mycir(draw, x, y, r)
+##        x = self.x0 - self.r0 * 2/5
+##        y = self.y0
+##        r = self.r0 * 3/5
+##        self.mycir(draw, x, y, r)
+##        x = self.x0 + self.r0 - self.r0 * 2/5 * 3/5 - self.r0 * 2/5 * 2/5 * 2
+##        y = self.y0
+##        r = self.r0 * 2/5 * 3/5
+##        self.mycir(draw, x, y, r)
+##        x = self.x0 + self.r0 - self.r0 * 2/5 * 2/5
+##        y = self.y0
+##        r = self.r0 * 2/5 * 2/5
+##        self.mycir(draw, x, y, r)
+##
+##        x = self.x0 + self.r0 * 2/5 * 4/5 + self.r0 * 1/5 * 1/5 * 0.6 # approx!
+##        y = self.y0 - self.r0 * 3/5 + self.r0 * 1/5 * 2/5 - self.r0 * 1/5 * 1/5 * 1.18 # approx!
+##        r = self.r0 * 2/5 * 4/5 + self.r0 * 1/5 * 1/5 * 0.4 # approx!
+##        self.mycir(draw, x, y, r)
+##
+##        x = self.x0 + self.r0 * 2/5 * 4/5 + self.r0 * 1/5 * 1/5 * 0.6 # approx!
+##        y = self.y0 + self.r0 * 3/5 - self.r0 * 1/5 * 2/5 + self.r0 * 1/5 * 1/5 * 1.18 # approx!
+##        r = self.r0 * 2/5 * 4/5 + self.r0 * 1/5 * 1/5 * 0.4 # approx!
+##        self.mycir(draw, x, y, r)
+
+        im.save(image)
+        self.dt += 1
+
+# 3 black strips divided by white ones, center white then 8 rows each diff angle speed and variable len (same angle)
+class Anim4():
+    mar = 56 # margin
+    count = 8 # count of circles
+    strips = 3 # count of strips
+    r0 = 48 # inner radius (white patch)
+    bg = (255, 255, 255) # bg color
+
+    def _init__(self):
+        self.init((0, 0), 0)
+
+    def init(self, canvas):
+        self.dt = 0
+        self.canvas = canvas
+
+    def drawframe(self, image):
+        im = Image.new('RGB', self.canvas, self.bg)
+        draw = ImageDraw.Draw(im)
+        dr = (self.canvas[0]/2-self.mar-self.r0)/self.count # assume canvas w=h
+        da = 360/2/self.strips
+        for x in range(self.count):
+            drx = dr*(x+1)
+            #drx = dr*x
+            xy = [(self.mar+drx,self.mar+drx), (self.canvas[0]-self.mar-drx,self.canvas[1]-self.mar-drx)]
+            draw.pieslice(xy, 0, 360, fill=self.bg, outline=None)
+            af = self.dt*(self.count-x)
+            for n in range(self.strips):
+                start = da*(2*n+0)+af
+                end = da*(2*n+1)+af
+                draw.pieslice(xy, start, end, fill=(0,0,0), outline=None)
+        draw.pieslice([(self.canvas[0]/2-self.r0/2, self.canvas[1]/2-self.r0/2), (self.canvas[0]/2+self.r0/2, self.canvas[1]/2+self.r0/2)], 0, 360, fill=(255,255,255), outline=None)
+
+        im.save(image)
+        self.dt += 1
+
 # ---
 
-def main():
-    image = 'tmp.png'   #tmp img file
-    #fcc = -1
-    #fcc = cv2.VideoWriter_fourcc(*"XVID")
-    fcc = cv2.VideoWriter_fourcc(*"MJPG")
-
+def do_anim1(fcc):
     o = Anim1()
     video_name = 'anim-01-video.avi'
     canvas = (512, 512)
@@ -169,8 +307,7 @@ def main():
     cv2.destroyAllWindows()
     video.release()
 
-# ---
-
+def do_anim2(fcc):
     o = Anim2()
     video_name = 'anim-02-video.avi'
     canvas = (512, 512)
@@ -187,6 +324,55 @@ def main():
     cv2.destroyAllWindows()
     video.release()
 
+def do_anim3(fcc):
+    o = Anim3()
+
+    canvas = (2480, 2480)
+    o.init(canvas, 5, 44)
+    o.drawframe('steiner_chain-one.png') # one
+
+#    video_name = 'anim-03-video.avi'
+#    canvas = (512, 512)
+#    video = cv2.VideoWriter(video_name, fcc, 25, canvas)
+
+#    steps = 25
+#    o.init(canvas, 5, 4)
+#    for n in range(steps):
+#        o.drawframe(image)
+#        ima = cv2.imread(image)
+#        video.write(ima)
+
+#    cv2.destroyAllWindows()
+#    video.release()
+
+def do_anim4(fcc):
+    o = Anim4()
+
+    video_name = 'anim-04-video.avi'
+    canvas = (512, 512)
+    video = cv2.VideoWriter(video_name, fcc, 25, canvas)
+
+    steps = 300
+    o.init(canvas)
+    for n in range(steps):
+        o.drawframe(image)
+        ima = cv2.imread(image)
+        video.write(ima)
+
+    cv2.destroyAllWindows()
+    video.release()
+
+# ---
+
+def main():
+    #fcc = -1
+    #fcc = cv2.VideoWriter_fourcc(*"XVID")
+    fcc = cv2.VideoWriter_fourcc(*"MJPG")
+
+    do_anim1(fcc)
+    do_anim2(fcc)
+#    do_anim3(fcc)
+    do_anim4(fcc)
 
 if __name__ == '__main__':
     main()
