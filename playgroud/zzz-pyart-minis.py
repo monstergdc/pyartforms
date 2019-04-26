1d516a9c1781aad5e9ba2114d0d7beb93b6f5661
Jakub Noniewicz, MoNsTeR of GDC / Noniewicz.com <32686195+monstergdc@users.noreply.github.com>
2019-04-26 18:59:26 +0200

    minis added

---
+++ playgroud/zzz-pyart-minis.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# paint algorithms (artificial artist) in Python - demo
+# (c)2018-2019 MoNsTeR/GDC, Noniewicz.com, Jakub Noniewicz
+# cre: 20181020
+# upd: 20190105, 06, 13, 18, 19, 22
+# upd: 20190302, 03, 30
+# upd: 20190414, 17, 21, 22, 24, 26
+
+# miniatures renderer
+
+
+import os
+from datetime import datetime as dt
+
+from drawtools import get_canvas, art_painter
+from life1 import life
+from lissajous import lissajous, lissajous_loop
+from waves import *
+from astroart import *
+from mandelbrot import generate_mandelbrot
+from smears import *
+from pyart_defs import *
+
+
+start_time = dt.now()
+root = 'minis' #linux
+if not os.path.exists(root):
+    os.makedirs(root)
+odir = root+'/' #linux
+
+
+def do_mazy(cnt, w, h, odir, name):
+    if name in predefs:
+        pr = predefs[name]
+        p = pr(w, h)
+    #else: ?
+
+    for n in range(cnt):
+        for i in range(len(p)):
+            p[i]['alpha'] = True #test
+            art_painter(p[i], odir+'%s-%dx%d-%02d.png' % (p[i]['name'], w, h, i+1))
+
+# --- go
+
+w, h = get_canvas('256')
+cnt = 1
+for m in predef_names:
+    do_mazy(cnt, w, h, odir, m)
+    
+time_elapsed = dt.now() - start_time
+print('ALL done. elapsed time: {}'.format(time_elapsed))
+
