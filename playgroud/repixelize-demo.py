d68f3a15f5dbd20eb82c141613dfdd4db446d0e4
Jakub Noniewicz, MoNsTeR of GDC / Noniewicz.com <32686195+monstergdc@users.noreply.github.com>
2020-12-12 18:02:23 +0100

    some updates to repixelize

--- playgroud/repixelize-demo.py
+++ playgroud/repixelize-demo.py
@@ -3,36 +3,23 @@
 
 # PyArtForms - Repixelize algorithm (artificial artist), v1.0, Python version - DEMO
 # (c)2018-2020 MoNsTeR/GDC, Noniewicz.com, Jakub Noniewicz
-# cre: 20201208, 10, 11
+# cre: 20201208, 10, 11, 12
 
 
 import os, sys
 from repixelize import *
 
 
-start_time = dt.now()
-root = '!output-repixel-test'
-if not os.path.exists(root):
-    os.makedirs(root)
-odir = root+'\\'
-indir = 'repixel-in\\'
-
-w, h = get_canvas('A4') # test4
-#w, h = get_canvas('A3') # test3
-#w, h = get_canvas('A2') # test2
-#w, h = get_canvas('A1') # final
-
-# ---
 
 def demo_looper(params, files, modes):
+    if 'postfix' in params:
+        ps = "-"+str(params['postfix'])
+    else:
+        ps = ""
     for fn in files:
         params['infile'] = indir+fn
         for mo in modes:
             params['mode'] = mo
-            if 'postfix' in params:
-                ps = "-"+str(params['postfix'])
-            else:
-                ps = ""
             params['outfile'] = odir+fn+'-repixel-'+mo+ps+'.png'
             start_time1 = dt.now()
             repix(params)
@@ -41,34 +28,83 @@ def demo_looper(params, files, modes):
 
 # ---
 
-params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
-demo_looper(params, files=['test-src2.jpg'], modes=['brush', 'lines', 'poly', 'rect', 'circle'])
-params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': False, 'rmin': 78, 'rmax': 98, 'postfix': 'nornd'}
-demo_looper(params, files=['test-src2.jpg'], modes=['brush', 'lines', 'poly', 'rect', 'circle'])
-quit()
+# poly test - nn * flux
+def demo_poly_test():
+    files=['test-src1.png', 'test-src2.jpg']
+    params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
+    for nn in [3, 64]:
+        for flux in [0, 5, 40, 100]:
+            params['nn'] = nn
+            params['flux'] = flux
+            params['postfix'] = 'nn' + str(nn) + 'flux' + str(flux)
+            demo_looper(params, files=files, modes=['poly'])
 
-params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
+# brush test - nn * bs (100*100 interesting/unexpected)
+def demo_brush_test():
+    files=['test-src1.png', 'test-src2.jpg']
+    params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
+    for nn in [5, 60, 100]:
+        for bs in [5, 15, 40, 100]:
+            params['nn'] = nn
+            params['bs'] = bs
+            params['postfix'] = 'nn' + str(nn) + 'bs' + str(bs)
+            demo_looper(params, files=files, modes=['brush'])
 
-#note: ~time@A4: lines=1:30 brush=1:00, poly=0:30
+# lines test - ?
+def demo_lines_test():
+    files=['test-src1.png', 'test-src2.jpg']
+    params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
+    # todo: more params
+    demo_looper(params, files=files, modes=['lines'])
 
-f = 'zx\\'
-files = [f+'BARBAR.png', f+'CABAL.png', f+'Cobra.png', f+'JETPAC.png', f+'MNTYMOLE.png', f+'NOD_YSOD.png', f+'ROBOCOP1.png', f+'skatecrz.png', f+'STORM.png', f+'URIDIUM.png']
-demo_looper(params, files, modes=['brush', 'lines', 'poly'])
+# demo for private zx files
+def demo_private_zx():
+        f = 'zx\\'
+        files = [f+'BARBAR.png', f+'CABAL.png', f+'Cobra.png', f+'JETPAC.png', f+'MNTYMOLE.png', f+'NOD_YSOD.png', f+'ROBOCOP1.png', f+'skatecrz.png', f+'STORM.png', f+'URIDIUM.png']
+        params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
+        # todo: test/use new params
+        demo_looper(params, files, modes=['brush', 'lines', 'poly'])
 
-files = ['dragon07e.jpg', 'dragon08x.jpg', 'firemaster.jpg', 'greendevil.jpg', 'robo.jpg', 'zjawa.jpg']
-demo_looper(params, files, modes=['brush', 'lines', 'poly'])
+# demo for private files
+def demo_priv():
+    # todo: test/use new params
+    params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 1.0, 'rnd': True, 'rmin': 78, 'rmax': 98}
+    files = ['dragon07e.jpg', 'dragon08x.jpg', 'firemaster.jpg', 'greendevil.jpg', 'robo.jpg', 'zjawa.jpg']
+    demo_looper(params, files, modes=['brush', 'lines', 'poly'])
 
-demo_looper(params, files=['head.png', 'xor1.png', 'AlanW1.png'], modes=['brush', 'lines', 'poly', 'rect', 'circle'])
-demo_looper(params, files=['zz-2014-2020.png'], modes=['rect', 'poly'])
+    demo_looper(params, files=['head.png', 'xor1.png', 'AlanW1.png'], modes=['brush', 'lines', 'poly', 'rect', 'circle'])
+    demo_looper(params, files=['zz-2014-2020.png'], modes=['rect', 'poly'])
 
-params = {'w': w, 'h': h, 'bk': (255, 255, 255), 'coef': 0.8, 'scale': 0.5, 'rnd': True, 'rmin': 50, 'rmax': 250}
-demo_looper(params, files=['fromopenshot1zzz2-x.png', 'PIC_2548-cp-min.JPG'], modes=['poly', 'rect', 'lines', 'circle', 'brush'])
+    params = {'w': w, 'h': h, 'bk': (255, 255, 255), 'coef': 0.8, 'scale': 0.5, 'rnd': True, 'rmin': 50, 'rmax': 250}
+    demo_looper(params, files=['PIC_2548-cp-min.JPG'], modes=['poly', 'rect', 'lines', 'circle', 'brush'])
 
-params = {'w': w, 'h': h, 'bk': (255, 255, 255), 'coef': 0.95, 'scale': 0.05, 'rnd': True, 'rmin': 90, 'rmax': 140}
-demo_looper(params, files=['Zuza-orig.jpg', 'Zuza-popr1.jpg', 'Zuza-popr2.jpg'], modes=['poly', 'lines', 'circle', 'brush'])
+    params = {'w': w, 'h': h, 'bk': (255, 255, 255), 'coef': 0.95, 'scale': 0.05, 'rnd': True, 'rmin': 90, 'rmax': 140}
+    demo_looper(params, files=['Zuza-orig.jpg', 'Zuza-popr1.jpg', 'Zuza-popr2.jpg'], modes=['poly', 'lines', 'circle', 'brush'])
 
-params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 0.1, 'rnd': True, 'rmin': 15, 'rmax': 180}
-demo_looper(params, files=['rgbxxx-1024-test.png'], modes=['lines'])
+    params = {'w': w, 'h': h, 'bk': (0, 0, 0), 'coef': 0.9, 'scale': 0.1, 'rnd': True, 'rmin': 15, 'rmax': 180}
+    demo_looper(params, files=['rgbxxx-1024-test.png'], modes=['lines'])
+
+# ---
+
+# todo: nice main
+
+start_time = dt.now()
+root = '!output-repixel-test'
+odir = root+'/'
+indir = 'repixel-in/'
+if not os.path.exists(root):
+    os.makedirs(root)
+
+w, h = get_canvas('A4') # test4
+#w, h = get_canvas('A3') # test3
+#w, h = get_canvas('A2') # test2
+#w, h = get_canvas('A1') # final
+
+#demo_poly_test()
+#demo_brush_test()
+demo_lines_test()
+#demo_private_zx()
+#demo_priv()
 
 time_elapsed = dt.now() - start_time
 print('ALL done. elapsed time: {}'.format(time_elapsed))
--- playgroud/repixelize.py
+++ playgroud/repixelize.py
@@ -8,21 +8,21 @@
 # cre: 20190330
 # upd: 20190401, 02, 03, 15, 22, 23
 # upd: 20200724, 25, 30
-# upd: 20201208, 10, 11
+# upd: 20201208, 10, 11, 12
 
 # note: input must be 24-bit RGB
 
-# TODO: xy scale sync issue | more params
+# TODO: xy scale sync issue | more params for lines mode
 
 
-import random, math, os, sys
+import random, math
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from drawtools import *
 
 
 def repix(params):
     """
-    'Repixelize' - remake small image into big one, change each pixel to bigger predefined multipixel form
+    'Repixelize' - remake small image into big one, change each pixel to bigger predefined but variable multipixel shape
     """
     def rect_():
         rect(d, int(x*dx), int(y*dy), int(dx*coef), int(dy*coef), fill=cin, outline=None)
@@ -31,19 +31,40 @@ def repix(params):
     def poly_():
         po = []
         r = int(dy/2*coef)
-        nn = 64 # par
+        if 'nn' in params:  # 'nn' - count of poly sides, bigger == smoother, default 64
+            nn = int(params['nn'])
+            if nn < 3:
+                nn = 3
+        else:
+            nn = 64
+        if 'flux' in params:  # 'flux' - random poly points variation (absoulte values), 0 == no variation, default 40
+            flux = int(params['flux'])
+        else:
+            flux = 40
         for i in range(nn):
-            rfx = random.randint(-40, 40) # par
-            rfy = random.randint(-40, 40) # par
+            rfx = random.randint(-flux, flux)
+            rfy = random.randint(-flux, flux)
             a = c*i/nn*360
             x1 = int(x*dx+(r+rfx)*math.cos(a))
             y1 = int(y*dy+(r+rfy)*math.sin(a))
             po.append((x1, y1))
         d.polygon(po, fill=cin, outline=None)
     def brush_():
-        for b in range(30+random.randint(0, 60)): # par
-            bs = 2+random.randint(0, 15) # par
-            if random.randint(0, 100) > 90: # par, opt - exceed dest rect
+        if 'bs' in params: # bruch rect block size variation, default 15
+            bs0 = int(params['bs'])
+            if bs0 < 0:
+                bs0 = 0
+        else:
+            bs0 = 15
+        if 'nn' in params:  # 'nn' - max extra count of rects, default 60
+            nn = int(params['nn'])
+            if nn < 0:
+                nn = 0
+        else:
+            nn = 60
+        for b in range(30+random.randint(0, nn)):
+            bs = 2+random.randint(0, bs0)
+            if random.randint(0, 100) > 90: # sometimes exceed dest rect
                 ofsx = random.randint(int(-dx), int(dx*2))
                 ofsy = random.randint(int(-dy), int(dy*2))
             else:
@@ -51,6 +72,9 @@ def repix(params):
                 ofsy = random.randint(0+1, int(dy)-1)
             rect(d, int(x*dx+ofsx), int(y*dy+ofsy), int(bs), int(bs), fill=cin, outline=None)
     def lines_():
+        horizontal = False
+        if 'horizontal' in params:
+            horizontal = params['horizontal']
         for b in range(random.randint(25, 25+55)): # par
             bs = random.randint(1, 6) # par
             addx1 = 0
@@ -62,10 +86,6 @@ def repix(params):
                 addx1 = -dx * random.randint(1, ex)
             if random.randint(0, 100) >= 80: #par?
                 addx2 = dx * random.randint(1, ex)
-            horizontal = False
-            if 'horizontal' in params:
-                if params['horizontal'] == True:
-                    horizontal = True
             if not horizontal:
                 if random.randint(0, 100) > 80: #par?
                     addy1 = -dy * random.randint(1, ex)
@@ -74,15 +94,32 @@ def repix(params):
             points = [x*dx+addx1+random.randint(0+1, int(dx)-1), y*dy+addy1+random.randint(0+1, int(dy)-1), x*dx+addx2+random.randint(0+1, int(dx)-1), y*dy+addy2+random.randint(0+1, int(dy)-1)]
             d.line(points, fill=cin, width=bs)
 
-    c = math.pi/180
-    w = params['w']
-    h = params['h']
+    if not 'infile' in params:
+        print("Source image (inflie) not given")
+        return
+    if not 'outfile' in params:
+        print("Destination image (outfile) not given")
+        return
     try:
         src = Image.open(params['infile'])
     except:
-        print("Error opening file:", params['infile'])
+        print("Error opening source image:", params['infile'])
         return
     outfile = params['outfile']
+    if not 'w' in params:
+        print("Destination width not given")
+        return
+    if not 'h' in params:
+        print("Destination height not given")
+        return
+    w = int(params['w'])
+    h = int(params['h'])
+    if h <= 0:
+        print("Destination height must be > 0")
+        return
+    if w <= 0:
+        print("Destination width must be > 0")
+        return
     if 'scale' in params:
         scale = params['scale']
         if scale != 1.0:
@@ -91,23 +128,23 @@ def repix(params):
         bk = params['bk']
     else:
         bk = (0,0,0)
-    if 'nn' in params:
-        nn = params['nn']
+    if 'coef' in params: # dot radius coefficient % (1.0 = 100%), default 0.9
+        coef = params['coef']
     else:
-        nn = 0
+        coef = 0.9
     img = Image.new('RGB', (w, h), color = bk)
-    coef = params['coef'] # note: dot radius coefficient %
     srcw = src.width
     srch = src.height
+    c = math.pi/180
     random.seed()
-    print('repix', params['mode'], params['infile'], '->', outfile)
+    print('repix', params['mode'], params['infile'], '->', outfile, '- resize:', (srcw, srch), '->', (w, h))
     d = ImageDraw.Draw(img)
     dx = w/srcw
     dy = h/srch
     for x in range(srcw):
         for y in range(srch):
             cin = src.getpixel((x, y))
-            if 'rnd' in params:
+            if 'rnd' in params: # random coef variation in range rmin..rmax
                 if params['rnd'] == True:
                     coef = float(random.randint(params['rmin'], params['rmax'])) / 100.0
             if params['mode'] == 'rect':
